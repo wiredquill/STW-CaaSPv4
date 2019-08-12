f86c30ba015bbdfec987204aa2cb0f80baa3ea79
WiredQuill <wiredquill@gmail.com>
2019-08-12 06:58:44 -0700

    Added sleep function after adding each node to let system sync

--- course_files/scripts/load_testing/install_caasp.sh
+++ course_files/scripts/load_testing/install_caasp.sh
@@ -9,23 +9,25 @@ sudo zypper in -y kubernetes-client skuba
 
 
 ## Create the cluster config file
-cd ~
-
+echo Creating Cluster Config
 skuba cluster init --control-plane master.example.com my-cluster
 
 ## Bootstrap the cluster with master01 as the only master node
+echo Bootstrapping Cluster
 cd my-cluster
 skuba node bootstrap --target master.example.com master01
+sleep 15s
 
 ## add worker10 as worker node
+echo Adding worker10
 skuba node join --role worker --target worker10.example.com worker10
+sleep 15s
 
 ## add worker11 as worker node
+echo Adding worker10
 skuba node join --role worker --target worker11.example.com worker11
 
 skuba cluster status
 
 mkdir ~/.kube
 cp ~/my-cluster/admin.conf ~/.kube/config
-
-
