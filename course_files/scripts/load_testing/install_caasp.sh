76733b3e13084475c10f8fb9e345de1f9b0df60a
WiredQuill <wiredquill@gmail.com>
2019-08-11 17:40:28 -0700

    Changed ##

--- course_files/scripts/load_testing/install_caasp.sh
+++ course_files/scripts/load_testing/install_caasp.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-/load the SSh agent and Keys
+## load the SSh agent and Keys
 eval "$(ssh-agent -s)"eval "$(ssh-agent -s)"
 ssh-add
 
-//install skuba so you can install CaaS Platform and the kubernetes Client
+## install skuba so you can install CaaS Platform and the kubernetes Client
 sudo zypper in -y kubernetes-client skuba
 
 
-//Create the cluster config file
+## Create the cluster config file
 cd
 
 skuba cluster init --control-plane master.example.com my-cluster
 
-//Bootstrap the cluster with master01 as the only master node
+## Bootstrap the cluster with master01 as the only master node
 cd my-cluster
 skuba node bootstrap --target master.example.com master01
 
-//add worker10 as worker node
+## add worker10 as worker node
 skuba node join --role worker --target worker11.example.com worker11
 
-//add worker11 as worker node
+## add worker11 as worker node
 skuba node join --role worker --target worker11.example.com worker11
 
 skuba cluster status
