5ce59c9d6f59c2ccc41556c08ad87d67f20025dc
WiredQuill <wiredquill@gmail.com>
2019-08-19 09:39:43 +0530

    Updated config_helm script with pauses for tiller to load

--- course_files/scripts/testing/config_helm.sh
+++ course_files/scripts/testing/config_helm.sh
@@ -9,6 +9,15 @@ kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceac
 
 helm init --tiller-image registry.suse.com/caasp/v4/helm-tiller:2.8.2 --service-account tiller
 
+echo sleeping for 30 secs for tiller to load
+
+sleep 30
+
+kubectl get pods --all-namespaces
+
+sleep 30
+
+
 helm repo add suse-charts https://kubernetes-charts.suse.com
 
 helm install suse-charts/log-agent-rsyslog --name 1.0.1 --set server.host=rsyslog-server.default.svc.cluster.local --set server.port=514
