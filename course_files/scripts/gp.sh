3b9bd421c0f39f3b9dcc0b0bd3ac330bb2da2f71
WiredQuill <wiredquill@gmail.com>
2019-08-22 09:53:46 +0530

    Created gp.sh to pull from git

---
+++ course_files/scripts/gp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## This script is used to pull the latest version of the the Course files from GitHub
+## copy this script to /usr/local/bin
+## chmod + x /usr/local/bin/gp.sh
+
+
+cd ~/STW-CaaSPv4
+git pull
