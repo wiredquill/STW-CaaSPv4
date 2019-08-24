e0cca7ebd2c700f5d43dbcddc9c43f5cd17e98bb
WiredQuill <wiredquill@gmail.com>
2019-08-24 14:33:05 +0800

    created rmt sh files

---
+++ course_files/scripts/rmt-sync-disable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo rmt-cli repo disable 3263 
+sudo rmt-cli repo disable 3261
+sudo rmt-cli repo disable 3353
+sudo rmt-cli repo disable 3351 
+sudo rmt-cli repo disable 3283
+sudo rmt-cli repo disable 3281
+sudo rmt-cli repo disable 3443
+sudo rmt-cli repo disable 3441
+sudo rmt-cli repo disable 3303
+sudo rmt-cli repo disable 3301
+sudo rmt-cli repo disable 3218
+sudo rmt-cli repo disable 3215
+sudo rmt-cli repo disable 3580
+sudo rmt-cli repo disable 3578
+
---
+++ course_files/scripts/rmt-sync-enable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo rmt-cli repo enable 3263 
+sudo rmt-cli repo enable 3261
+sudo rmt-cli repo enable 3353
+sudo rmt-cli repo enable 3351 
+sudo rmt-cli repo enable 3283
+sudo rmt-cli repo enable 3281
+sudo rmt-cli repo enable 3443
+sudo rmt-cli repo enable 3441
+sudo rmt-cli repo enable 3303
+sudo rmt-cli repo enable 3301
+sudo rmt-cli repo enable 3218
+sudo rmt-cli repo enable 3215
+sudo rmt-cli repo enable 3580
+sudo rmt-cli repo enable 3578
+
