3a4134abe4f38a373c03aee2a569bce0dd49f470
WiredQuill <wiredquill@gmail.com>
2019-09-03 07:05:23 -0700

    Added v5 to install script

--- course_files/scripts/testing/install_caasp.sh
+++ course_files/scripts/testing/install_caasp.sh
@@ -15,17 +15,17 @@ skuba cluster init --control-plane master.example.com my-cluster
 ## Bootstrap the cluster with master01 as the only master node
 echo Bootstrapping Cluster
 cd my-cluster
-skuba node bootstrap --target master.example.com master01
+skuba node bootstrap -v5 --target master.example.com master01
 sleep 15s
 
 ## add worker10 as worker node
 echo Adding worker10
-skuba node join --role worker --target worker10.example.com worker10
+skuba node join -v5 --role worker --target worker10.example.com worker10
 sleep 15s
 
 ## add worker11 as worker node
 echo Adding worker10
-skuba node join --role worker --target worker11.example.com worker11
+skuba node join -v5 --role worker --target worker11.example.com worker11
 
 skuba cluster status
 
