80df5cf5975e157ed4a00b73beb4063078d3bcd5
WiredQuill <wiredquill@gmail.com>
2019-09-05 08:20:40 -0700

    Changes enable to disable

--- course_files/scripts/rmt-sync-disable.sh
+++ course_files/scripts/rmt-sync-disable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo rmt-cli product enable 1772
-sudo rmt-cli product enable 1790
-sudo rmt-cli product enable 1776
-sudo rmt-cli product enable 1808
-sudo rmt-cli product enable 1780
-sudo rmt-cli product enable 1863
-sudo rmt-cli product enable 1763
+sudo rmt-cli product disable 1772
+sudo rmt-cli product disable 1790
+sudo rmt-cli product disable 1776
+sudo rmt-cli product disable 1808
+sudo rmt-cli product disable 1780
+sudo rmt-cli product disable 1863
+sudo rmt-cli product disable 1763
