7b02446601771e4cf6b5e300e78b919c46d98f79
WiredQuill <wiredquill@gmail.com>
2019-09-05 08:17:03 -0700

    Change rmt enable and disable scripts to use products instead of repos

--- course_files/scripts/rmt-sync-disable.sh
+++ course_files/scripts/rmt-sync-disable.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-sudo rmt-cli repo disable 3263 
-sudo rmt-cli repo disable 3261
-sudo rmt-cli repo disable 3353
-sudo rmt-cli repo disable 3351 
-sudo rmt-cli repo disable 3283
-sudo rmt-cli repo disable 3281
-sudo rmt-cli repo disable 3443
-sudo rmt-cli repo disable 3441
-sudo rmt-cli repo disable 3303
-sudo rmt-cli repo disable 3301
-sudo rmt-cli repo disable 3218
-sudo rmt-cli repo disable 3215
-sudo rmt-cli repo disable 3580
-sudo rmt-cli repo disable 3578
-
+sudo rmt-cli product enable 1772
+sudo rmt-cli product enable 1790
+sudo rmt-cli product enable 1776
+sudo rmt-cli product enable 1808
+sudo rmt-cli product enable 1780
+sudo rmt-cli product enable 1863
+sudo rmt-cli product enable 1763
--- course_files/scripts/rmt-sync-enable.sh
+++ course_files/scripts/rmt-sync-enable.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-sudo rmt-cli repo enable 3263 
-sudo rmt-cli repo enable 3261
-sudo rmt-cli repo enable 3353
-sudo rmt-cli repo enable 3351 
-sudo rmt-cli repo enable 3283
-sudo rmt-cli repo enable 3281
-sudo rmt-cli repo enable 3443
-sudo rmt-cli repo enable 3441
-sudo rmt-cli repo enable 3303
-sudo rmt-cli repo enable 3301
-sudo rmt-cli repo enable 3218
-sudo rmt-cli repo enable 3215
-sudo rmt-cli repo enable 3580
-sudo rmt-cli repo enable 3578
-
+sudo rmt-cli product enable 1772
+sudo rmt-cli product enable 1790
+sudo rmt-cli product enable 1776
+sudo rmt-cli product enable 1808
+sudo rmt-cli product enable 1780
+sudo rmt-cli product enable 1863
+sudo rmt-cli product enable 1763
