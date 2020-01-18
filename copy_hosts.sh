6fdd384aca1e99978b460c72d405c5577251a684
WiredQuill <wiredquill@gmail.com>
2020-01-17 16:55:11 -0800

    Created copy hosts

---
+++ copy_hosts.sh
@@ -0,0 +1,7 @@
+scp -i  hosts ec2-user@master1.susetech.org:~ 
+scp -i  hosts ec2-user@worker1.susetech.org:~ 
+scp -i  hosts ec2-user@worker2.susetech.org:~ 
+
+ssh -i ec2-user@master1.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot) &>/dev/null &"
+ssh -i ec2-user@worker1.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot) &>/dev/null &"
+ssh -i ec2-user@worker2.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot) &>/dev/null &"
