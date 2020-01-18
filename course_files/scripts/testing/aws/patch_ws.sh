26187a394d7333a5711b553a6f424722637d2f7a
WiredQuill <wiredquill@gmail.com>
2020-01-18 08:42:04 -0800

    Updated scp of hosts

--- course_files/scripts/testing/aws/patch_ws.sh
+++ course_files/scripts/testing/aws/patch_ws.sh
@@ -2,8 +2,11 @@
 
 # This copies the keys to the Workstation
 
-scp -i "susetech-k8s-keypair.pem" susetech-k8s-keypair.pub ec2-user@workstation.susetech.org:/home/ec2-user/.ssh/id_rsa.pub
-scp -i "susetech-k8s-keypair.pem" susetech-k8s-keypair.pem ec2-user@workstation.susetech.org:/home/ec2-user/.ssh/id_rsa
+scp -i "susetech-k8s-keypair.pem" susetech-k8s-keypair.pub ec2-user@workstation.susetech.org:/~
+scp -i "susetech-k8s-keypair.pem" susetech-k8s-keypair.pem ec2-user@workstation.susetech.org:/~
+
+ssh -i "susetech-k8s-keypair.pem" ec2-user@workstation.susetech.org "sudo cp /home/ec2-user/susetech-k8s-keypair.pub /home/ec2-user/.ssh/id_rsa.pub"
+ssh -i "susetech-k8s-keypair.pem" ec2-user@workstation.susetech.org "sudo cp /home/ec2-user/susetech-k8s-keypair.pem /home/ec2-user/.ssh/id_rsa"
 
 # ssh -i "susetech-k8s-keypair.pem" ec2-user@workstation.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot) &>/dev/null &"
 
