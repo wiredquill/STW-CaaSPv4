ff7acb7cf41673d2ba8f6a657c8d3c677b9d53de
WiredQuill <wiredquill@gmail.com>
2020-01-18 09:10:27 -0800

    Added Pauses to patch nodes

--- course_files/scripts/testing/aws/patch_nodes.sh
+++ course_files/scripts/testing/aws/patch_nodes.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-scp -i  hosts ec2-user@master1.susetech.org:~
-scp -i  hosts ec2-user@worker1.susetech.org:~
-scp -i  hosts ec2-user@worker2.susetech.org:~
+echo master1.susetech.org
+ping -1 master1.susetech.org
+ping -1 master1
+echo .
+echo worker1.susetech.org
+ping worker1.susetech.org
+ping worker1
+echo .
+echo worker2.susetech.org
+ping worker2.susetech.org
+ping worker2
+
+read -p "Press [Enter] key to start update..."
+
+scp  hosts ec2-user@master1.susetech.org:~
+scp  hosts ec2-user@worker1.susetech.org:~
+scp  hosts ec2-user@worker2.susetech.org:~
 
 #ssh -i "susetech-k8s-keypair.pem" ec2-user@workstation.susetech.org "sudo zypper ref &&  sudo zypper up -y && sudo reboot"
-
-ssh -i ec2-user@master1.susetech.org “sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot"
-ssh -i ec2-user@worker1.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot"
-ssh -i ec2-user@worker2.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot"
+echo master1.susetech.org
+ssh ec2-user@master1.susetech.org “sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot"
+read -p "Press [Enter] key to start update..."
+echo worker1.susetech.org
+ssh ec2-user@worker1.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot"
+echo worker1.susetech.org
+ssh ec2-user@worker2.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot"
 
 #ssh -i ec2-user@master1.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot) &>/dev/null &"
 #ssh -i ec2-user@worker1.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot) &>/dev/null &"
