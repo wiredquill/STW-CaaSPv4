ad8ac43f54da27f96188dbec57d62fca458b9379
WiredQuill <wiredquill@gmail.com>
2020-01-18 07:18:17 -0800

    Added Pause and scp

--- course_files/scripts/testing/aws/remove_keys.sh
+++ course_files/scripts/testing/aws/remove_keys.sh
@@ -7,3 +7,11 @@ ssh-keygen -R    worker2.susetech.org
 ssh-keygen -R    master1.susetech.org
 ssh-keygen -R    ws.susetech.org
 ssh-keygen -R    workstation.susetech.org
+
+pause
+scp -i "susetech-k8s-keypair.pem" susetech-k8s-keypair.pub ec2-user@ws.susetech.org:/home/ec2-user/.ssh/id_rsa.pub
+
+pause
+scp -i "susetech-k8s-keypair.pem" susetech-k8s-keypair.pem ec2-user@ws.susetech.org:/home/ec2-user/.ssh/id_rsa.pem
+
+# ssh -i "susetech-k8s-keypair.pem" ec2-user@workstation.susetech.org “(sudo zypper ref &&  sudo zypper up -y && sudo cp /home/ec2-user/hosts /etc/hosts && sudo reboot) &>/dev/null &"
