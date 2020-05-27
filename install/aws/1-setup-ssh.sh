2b79ea3ccd95c3f765929d1c727f9fdcc08230c4
WiredQuill <wiredquill@gmail.com>
2020-05-27 16:49:57 -0700

    Added chmod

--- install/aws/1-setup-ssh.sh
+++ install/aws/1-setup-ssh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp ~/STW-CaaSPv4/install/aws/susetech-k8s-keypair.pem ~/.ssh
+sudo chown ec2-user:users ~/.ssh/susetech-k8s-keypair.pem
+sudo chmod 600 ec2-user:users ~/.ssh/susetech-k8s-keypair.pem
 eval $(ssh-agent -s)
 ssh-add
 ssh-add ~/.ssh/susetech-k8s-keypair.pem
